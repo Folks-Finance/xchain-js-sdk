6df0b2e10e64b40802268e1e7b544e4197cb9906
Danylo Moshenskyi <moshenskyi.danylo@gmail.com>
2025-01-06 15:54:41 +0100

    docs(examples): stable/variable rate calculation

--- examples/borrow.ts
+++ examples/borrow.ts
@@ -55,16 +55,25 @@ async function main() {
   const accountId = "0x7d6...b66" as AccountId; // Your xChainApp account id
   const loanId = "0x166...c12" as LoanId; // Your loan id
   const amountToBorrow = parseUnits("0.0005", 18); // 0.0005 BNB (BNB has 18 decimals)
-  const poolInfo = await FolksPool.read.poolInfo(tokenId);
-  const interestRate = poolInfo.stableBorrowData.interestRate[0];
-  const [maxStableRate] = dn.mul(interestRate, 1.05); // 5% max deviation from current rate
+  const isStableRate = false;
+
+  let maxStableRate;
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  if (isStableRate) {
+    const poolInfo = await FolksPool.read.poolInfo(tokenId);
+    const interestRate = poolInfo.stableBorrowData.interestRate[0];
+    const [rateWithSlippage] = dn.mul(interestRate, 1.05); // 5% max deviation from current rate
+    maxStableRate = rateWithSlippage;
+  } else {
+    maxStableRate = BigInt(0);
+  }
 
   const prepareBorrowCall = await FolksLoan.prepare.borrow(
     accountId,
     loanId,
     tokenId,
     amountToBorrow,
-    maxStableRate, // Use BigInt(0) for variable rate
+    maxStableRate,
     chain,
     adapters,
   );
@@ -73,7 +82,7 @@ async function main() {
     loanId,
     tokenId,
     amountToBorrow,
-    maxStableRate, // Use BigInt(0) for variable rate
+    maxStableRate,
     chain,
     prepareBorrowCall,
   );
